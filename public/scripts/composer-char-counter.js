d035cac0e227cd000b74f94b183243b948043fa3
Ben Deltenre <benjdelt@gmail.com>
2018-08-14 19:32:42 +0000

    Update counter and counter style

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-  $(".new-tweet textarea").on("keypress", function(event){
-    console.log(140 - this.value.length);
+  $(".new-tweet textarea").on("keyup", function(event){
+    let count = 140 - this.value.length;
+    $(this).siblings(".counter").html(count);
+    if (count < 0) {
+      $(this).siblings(".counter").addClass("negative");
+    }
+    if (count > 0) {
+      $(this).siblings(".counter").removeClass("negative");
+    }
   });
+
 });
