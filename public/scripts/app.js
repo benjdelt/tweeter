eceb1560387093b140d87a61e9fde3b1bcaa220d
Ben Deltenre <benjdelt@gmail.com>
2018-08-15 20:40:19 +0000

    Added button style and functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,95 +8,104 @@
 
 $(function() {
 
-// Turns JSON object from database into html
+  // Compose button slide new-tweet form up or down
 
-function addElement(tag, className, content) {
-   return $(tag).addClass(className).text(content);
-}
-
-// Calaculates time elapsed since (from Stackoverflow)
-
-function timeSince(date) {
-
-  let seconds = Math.floor((new Date() - date) / 1000);
-
-  let interval = Math.floor(seconds / 31536000);
-
-  if (interval > 1) {
-    return interval + " years";
-  }
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
-    return interval + " months";
-  }
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
-    return interval + " days";
-  }
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
-    return interval + " hours";
-  }
-  interval = Math.floor(seconds / 60);
-  if (interval > 1) {
-    return interval + " minutes";
-  }
-  return Math.floor(seconds) + " seconds";
-}
-
-function createHeader(tweetObject) {
-  let $header = $("<header>");
-  let $avatar = $("<img>").attr("src", tweetObject.user.avatars.small);
-  $header.append($avatar);
-  let $accName = addElement("<h3>", "account-name", tweetObject.user.name);
-  $header.append($accName);
-  let $handle = addElement("<span>", "handle", tweetObject.user.handle);
-
-  $header.append($handle);
-  return $header;
-}
-
-function createBody(tweetObject) {
-  return addElement("<p>", "body", tweetObject.content.text);
-
-}
-
-function createFooter(tweetObject) {
-  let $footer = $("<footer>");
-  let $createdAt = addElement("<span>", "created-at", timeSince(tweetObject.created_at));
-  
-  $footer.append($createdAt);
-  let $icons = addElement("<span>", "icons");
-
-  let $flag = addElement("<i>", "material-icons", "flag");
-  $icons.append($flag);  
-  let $repeat = addElement("<i>", "material-icons", "repeat");
-  $icons.append($repeat);
-  let $favorite = addElement("<i>", "material-icons", "favorite");
-
-  $icons.append($favorite);
-  $footer.append($icons);
-  return $footer;
-}
-
-function createTweetElement(tweetObject) {
-  
-  let $tweet = $("<article>").addClass("tweet");
-
-  $tweet.append(createHeader(tweetObject));
-  $tweet.append(createBody(tweetObject));
-  $tweet.append(createFooter(tweetObject));
-
-  return $tweet;
-}
-
-// Render tweets to the browser
-
-function renderTweets(tweets) {
-  tweets = tweets.reverse();
-  return tweets.forEach(t => {
-    return $('#tweets-container').append(createTweetElement(t));
+  $('#compose').on('click', function() {
+    $('.new-tweet').slideToggle();
+    if($("textarea[name='text']").is(':visible')) {
+      $("textarea[name='text']").focus();
+    }  
   })
+
+  // Turns JSON object from database into html
+
+  function addElement(tag, className, content) {
+    return $(tag).addClass(className).text(content);
+  }
+
+  // Calaculates time elapsed since (from Stackoverflow)
+
+  function timeSince(date) {
+
+    let seconds = Math.floor((new Date() - date) / 1000);
+
+    let interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+      return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+  }
+
+  function createHeader(tweetObject) {
+    let $header = $("<header>");
+    let $avatar = $("<img>").attr("src", tweetObject.user.avatars.small);
+    $header.append($avatar);
+    let $accName = addElement("<h3>", "account-name", tweetObject.user.name);
+    $header.append($accName);
+    let $handle = addElement("<span>", "handle", tweetObject.user.handle);
+
+    $header.append($handle);
+    return $header;
+  }
+
+  function createBody(tweetObject) {
+    return addElement("<p>", "body", tweetObject.content.text);
+
+  }
+
+  function createFooter(tweetObject) {
+    let $footer = $("<footer>");
+    let $createdAt = addElement("<span>", "created-at", timeSince(tweetObject.created_at));
+    
+    $footer.append($createdAt);
+    let $icons = addElement("<span>", "icons");
+
+    let $flag = addElement("<i>", "material-icons", "flag");
+    $icons.append($flag);  
+    let $repeat = addElement("<i>", "material-icons", "repeat");
+    $icons.append($repeat);
+    let $favorite = addElement("<i>", "material-icons", "favorite");
+
+    $icons.append($favorite);
+    $footer.append($icons);
+    return $footer;
+  }
+
+  function createTweetElement(tweetObject) {
+    
+    let $tweet = $("<article>").addClass("tweet");
+
+    $tweet.append(createHeader(tweetObject));
+    $tweet.append(createBody(tweetObject));
+    $tweet.append(createFooter(tweetObject));
+
+    return $tweet;
+  }
+
+  // Render tweets to the browser
+
+  function renderTweets(tweets) {
+    tweets = tweets.reverse();
+    return tweets.forEach(t => {
+      return $('#tweets-container').append(createTweetElement(t));
+    })
 }
 
 // Fetch tweets with Ajax
